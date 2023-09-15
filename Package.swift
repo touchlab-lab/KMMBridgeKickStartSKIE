5761f5efd3aee807d2539d0059eb4b089426411d
runner <runner@Mac-1694787339539.local>
2023-09-15 14:50:32 +0000

    KMM SPM package release for 0.3.2

--- Package.swift
+++ Package.swift
@@ -1,7 +1,11 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://maven.pkg.github.com/touchlab/KMMBridgeKickStartSKIE/co/touchlab/kmmbridgekickstartskie/allshared-kmmbridge/0.3.2/allshared-kmmbridge-0.3.2.zip"
+let remoteKotlinChecksum = "897082c4d9105524d37c1a5c62a93d860b07396ffc794d7e00e13df4dd234f4a"
 let packageName = "allshared"
+// END KMMBRIDGE BLOCK
 
 let package = Package(
     name: packageName,
@@ -17,7 +21,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            path: "./allshared/build/XCFrameworks/debug/\(packageName).xcframework"
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
         )
         ,
     ]
